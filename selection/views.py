548ab93999ad4772de15ba39be72bc8cd633dbb3
Wes Bailey <espressoandcode@gmail.com>
2019-08-04 15:53:36 -0400

    Add date validation for main page, minor style tweaks.

--- selection/views.py
+++ selection/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from selection import services
 import requests
+from django.contrib import messages
 from selection.models import Circuits, DriverStandings, Drivers, LapTimes, Races, Results
 
 def home (request):
   if (request.method == 'GET'):
-    selectedYear = request.GET.get('year','2000')
-    races = Races.objects.filter(year=selectedYear)
-    return render(request, 'select.html', context={'races': races, 'year': selectedYear})
+    selectedYear = request.GET.get('year','1')
+    if selectedYear == '1':
+      return render(request, 'home.html')
+    elif (int(selectedYear) > 1949 and int(selectedYear) < 2019):
+      races = Races.objects.filter(year=selectedYear)
+      return render(request, 'select.html', context={'races': races, 'year': selectedYear})
+    else:
+      messages.info(request, 'Please enter a year between 1949 and 2019.')
+      selectedYear = None
+      return render(request, 'home.html')
 
 def results (request, year, raceId):
-  # if (request.method == 'GET'):
-    # selectedYear = request.GET.get('year','')
-    print("selectedYear - ", year)
+  if (request.method == 'GET'):
+    selectedYear = request.GET.get('year','')
     raceResults = []
     rawResults = Results.objects.filter(raceId=raceId)
     for item in rawResults:
       if (item.position == -1):
         item.position = 99
-        print(item.position)
       else:
         item.position = item.position
-        print(item.position)
       raceResults.append(item)
-
-    print("Result - ", raceResults)
-
-
     return render(request, 'results.html', context={'results': raceResults})
