bd3e23722eb0f31091ce77b7d3cc09555f722338
Wes Bailey <espressoandcode@gmail.com>
2019-08-12 22:26:30 -0400

    Style tweaks and code cleanup.

--- selection/services.py
+++ selection/services.py
@@ -1,9 +1,11 @@
-import requests
-
+import requests, os
 from bs4 import BeautifulSoup
 from googlesearch import search
 from requests.compat import urljoin
 from urllib.parse import urlencode
+
+seId = os.environ.get('SEARCH_ENGINE_ID')
+seKey = os.environ.get('CUSTOM_SEARCH_API_KEY')
 
 def getWikiData(url):
   page = requests.get(url)
@@ -15,13 +17,16 @@ def getWikiData(url):
   }
   return wikiData
 
-
 def getWebSearchResults(driver, year, race):
-  print(driver, year, race)
   searchParams ={ "q": [driver, str(year), race]}
   urlParams = urlencode(searchParams, doseq=True)
-  print("url - ", urlParams)
-  urlString = 'https://www.googleapis.com/customsearch/v1?q=' + urlParams + '&cx=016700597778999691736:clhkunuoizk&lr=lang_en&key=AIzaSyDIKEbCJ5dihD-1HoGFYo2eI-WaFtVhtD8'
-  print("urlString - ", urlString)
+  urlString = f'https://www.googleapis.com/customsearch/v1?q={urlParams}&cx={seId}&lr=lang_en&key={seKey}'
+  pages = requests.get(urlString).json()
+  return pages
+
+def getFrontPageResults():
+  searchParams ={ "q": "formula 1 today"}
+  urlParams = urlencode(searchParams)
+  urlString = f'https://www.googleapis.com/customsearch/v1?q={urlParams}&cx={seId}&lr=lang_en&key={seKey}'
   pages = requests.get(urlString).json()
   return pages
