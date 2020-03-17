eae63f7bcadca14d1709ca3f627273265be38e6c
Wes Bailey <espressoandcode@gmail.com>
2020-03-17 15:14:24 -0400

    Change debug mode.

--- formula1app/settings.py
+++ formula1app/settings.py
@@ -8,13 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'r_ed6rwnq9@qf(!ps4g$a6g*(dnczf$0o8s8z_gqz0@ag3cyeh'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'r_ed6rwnq9@qf(!ps4g$a6g*(dnczf$0o8s8z_gqz0@ag3cyeh'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['f1fan.azurewebsites.net']
 
